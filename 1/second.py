04018430b33a63687defb85b4c1236dc66a0ded1
gaplin <gaplinn@gmail.com>
2023-12-09 17:22:52 +0100

    1. day

---
+++ 1/second.py
@@ -0,0 +1,14 @@
+input = open("input/input.txt", "r").read().splitlines()
+
+sums = []
+current_value = 0
+for line in input:
+    if line == '':
+        sums.append(current_value)
+        current_value = 0
+    else:
+        current_value += int(line)
+
+sorted_sums = sorted(sums, reverse=True)
+
+print(sum(sorted_sums[:3]))
